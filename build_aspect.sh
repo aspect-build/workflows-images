5cd06a31accec7b0f9dc9d4b7337dcaaa2f4dff0
Greg Magolan <greg@aspect.build>
2026-03-04 21:12:12 -0800

    Expand AMI regions

--- build_aspect.sh
+++ build_aspect.sh
@@ -13,9 +13,12 @@ architectures=(
 aws_profile=workflows-images_AspectAdministration
 
 aws_regions=(
-  us-west-2
   us-east-1
   us-east-2
+  us-west-1
+  us-west-2
+  eu-central-1
+  eu-west-1
 )
 
 gcp_project=aspect-workflows-images
